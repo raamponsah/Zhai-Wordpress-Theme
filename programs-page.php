54fa350dc0d7fe08f712907093cfb8071d29d168
Ralph <ramponsah026@st.ug.edu.gh>
2022-12-10 12:55:52 +0000

    sddd

---
+++ programs-page.php
@@ -0,0 +1,41 @@
+<?php get_header('general-page') ?>
+<aside class="page-cta">
+    <div>
+        <h3>Re-defining: Hope To the African</h3>
+        <p>
+            There comes a nice little fluffer. This present moment is perfect simply due to the fact you're experiencing
+            it.
+        </p>
+    </div>
+    <div>
+        <a href="" class="button button-outline orange">JOIN US TODAY</a>
+    </div>
+</aside>
+
+<main>
+
+
+
+    <section id="programs">
+        <section class="program">
+            <div class="program-image">
+                <img src="" alt="">
+            </div>
+            <div class="program-content">
+                <h4 class="program-title">
+                    Supporting Youth Football
+                </h4>
+                <p>
+                    ZHAI, as part of its social enterprise duties, is supporting the youth football team in the
+                </p>
+            </div>
+        </section>
+    </section>
+
+
+</main>
+
+<?php get_template_part('includes/mission-sections-page'); ?>
+
+
+<?php get_footer() ?>
