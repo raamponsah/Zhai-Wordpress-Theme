7de9f693e31ddfd6a1108cad083df48b0ddeed92
Ralph <ramponsah026@st.ug.edu.gh>
2022-12-13 19:12:39 +0000

    fpage

---
+++ founder-page.php
@@ -0,0 +1,39 @@
+<?php
+
+// Template Name: Founder's Page
+
+get_header('general-page') ?>
+<aside class="page-cta">
+    <div>
+        <h3>Re-defining: Hope To the African</h3>
+        <p>
+            There comes a nice little fluffer. This present moment is perfect simply due to the fact you're experiencing
+            it.
+        </p>
+    </div>
+    <div>
+        <a href="" class="button button-outline orange">JOIN US TODAY</a>
+    </div>
+</aside>
+
+<main>
+
+
+
+
+
+
+    <?php if(have_posts()):while(have_posts()):the_post(); ?>
+    <h4><?php the_title(); ?></h4>
+
+    <?php the_content();?>
+
+    <?php endwhile;endif; ?>
+
+
+</main>
+
+<?php get_template_part('includes/mission-sections-page'); ?>
+
+
+<?php get_footer() ?>
