1811b3b10c13dcca7fbde9f498ea4a3db6cb3d2b
Ralph <ramponsah026@st.ug.edu.gh>
2022-12-14 00:58:08 +0000

    pol

--- contact-page.php
+++ contact-page.php
@@ -33,11 +33,13 @@ while (have_posts()):
             <?php endwhile; endif; ?>
         </div>
 
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d63523.94296793566!2d-0.20386067816194908!3d5.677473168069426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1scastle%20bridge%20street%20east%20adenta%20accra!5e0!3m2!1sen!2sgh!4v1670979083826!5m2!1sen!2sgh"
-            width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div>
 
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d63523.94296793566!2d-0.20386067816194908!3d5.677473168069426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1scastle%20bridge%20street%20east%20adenta%20accra!5e0!3m2!1sen!2sgh!4v1670979083826!5m2!1sen!2sgh"
+                width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
 
     </div>
 
