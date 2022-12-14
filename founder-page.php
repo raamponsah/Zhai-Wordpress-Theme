3625e7d057de8c1d8a5e5053f2c977944ed19f32
Ralph <ramponsah026@st.ug.edu.gh>
2022-12-14 11:09:15 +0000

    remo

--- founder-page.php
+++ founder-page.php
@@ -3,25 +3,16 @@
 // Template Name: Founder's Page
 
 get_header('general-page') ?>
-<aside class="page-cta redefine">
-    <div class="page-cta-text">
-        <div>
-            <h3>Re-defining: Hope To the African</h3>
-            <p>
-                There comes a nice little fluffer. This present moment is perfect simply due to the fact you're
-                experiencing
-                it.
-            </p>
-        </div>
-        <div>
-            <a href="" class="button button-outline orange">JOIN US TODAY</a>
-        </div>
+<aside class="page-cta">
+    <div>
+        <h3>Re-defining: Hope To the African</h3>
+        <p>
+            There comes a nice little fluffer. This present moment is perfect simply due to the fact you're experiencing
+            it.
+        </p>
     </div>
-
-    <div class="video-widget">
-        <?php if (is_active_sidebar('video-widget')): ?>
-        <?php dynamic_sidebar('video-widget'); ?>
-        <?php endif; ?>
+    <div>
+        <a href="" class="button button-outline orange">JOIN US TODAY</a>
     </div>
 </aside>
 
