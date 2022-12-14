7212bdf9f9fb28bae1facfeb5f2019a93243ef6d
Ralph <ramponsah026@st.ug.edu.gh>
2022-12-14 11:19:27 +0000

    new

--- home.php
+++ home.php
@@ -15,36 +15,36 @@ get_header('founder') ?>
             not only find but also keep a job. Improved literacy also leads to more informed health decisions about
             nutrition and preventative care, which benefits both the individual and the nation as a whole.
         </p>
-        </ </aside>
+    </aside>
 
-        <main>
+    <main>
 
 
 
-            <div class=" founder-video">
+        <div class=" founder-video">
 
-            </div>
-            <h3 class="spared-title">Articles by Zhai Founder</h3>
-            <div class="articles-wrapper">
-                <?php if (have_posts()):
+        </div>
+        <h3 class="spared-title">Articles by Zhai Founder</h3>
+        <div class="articles-wrapper">
+            <?php if (have_posts()):
             while (have_posts()):
                 the_post(); ?>
-                <div class="article">
-                    <div class="article-image">
-                        <a href="<?php the_permalink() ?>"><img src="<?php the_post_thumbnail() ?>" alt=""></a>
-                    </div>
-                    <a href="<?php the_permalink() ?>">
-                        <h4 class="article-title">
-                            <?php the_title(); ?>
-                        </h4>
-                    </a>
-                    <p>
-                        <?php the_excerpt() ?>
-                    </p>
+            <div class="article">
+                <div class="article-image">
+                    <a href="<?php the_permalink() ?>"><img src="<?php the_post_thumbnail() ?>" alt=""></a>
                 </div>
-                <?php endwhile; endif; ?>
-
+                <a href="<?php the_permalink() ?>">
+                    <h4 class="article-title">
+                        <?php the_title(); ?>
+                    </h4>
+                </a>
+                <p>
+                    <?php the_excerpt() ?>
+                </p>
             </div>
+            <?php endwhile; endif; ?>
+
+        </div>
 
 
 
@@ -53,9 +53,9 @@ get_header('founder') ?>
 
 
 
-        </main>
+    </main>
 
-        <?php get_template_part('includes/mission-sections-page'); ?>
+    <?php get_template_part('includes/mission-sections-page'); ?>
 
 
-        <?php get_footer() ?>
+    <?php get_footer() ?>
