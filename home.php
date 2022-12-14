6d486525596ffa90f9e735de225d131a499dddb2
Ralph <ramponsah026@st.ug.edu.gh>
2022-12-14 11:33:20 +0000

    aside

--- home.php
+++ home.php
@@ -2,49 +2,50 @@
 
 
 get_header('founder') ?>
+
+
 <aside class="pg">
-    <aside class="page-cta">
+
+    <h2>Zihao Zhuang</h2>
+
+    <p>
+        ZHAI believes that a person who is no longer stymied by reading a job application will have the skills
+        to
+        not only find but also keep a job. Improved literacy also leads to more informed health decisions about
+        nutrition and preventative care, which benefits both the individual and the nation as a whole.
+    </p>
+
+
+</aside>
+
+<main>
 
 
 
-        <h4>Zihao Zhuang</h4>
+    <div class=" founder-video">
 
-        <p>
-            ZHAI believes that a person who is no longer stymied by reading a job application will have the skills
-            to
-            not only find but also keep a job. Improved literacy also leads to more informed health decisions about
-            nutrition and preventative care, which benefits both the individual and the nation as a whole.
-        </p>
-    </aside>
-
-    <main>
-
-
-
-        <div class=" founder-video">
-
-        </div>
-        <h3 class="spared-title">Articles by Zhai Founder</h3>
-        <div class="articles-wrapper">
-            <?php if (have_posts()):
+    </div>
+    <h3 class="spared-title">Articles by Zhai Founder</h3>
+    <div class="articles-wrapper">
+        <?php if (have_posts()):
             while (have_posts()):
                 the_post(); ?>
-            <div class="article">
-                <div class="article-image">
-                    <a href="<?php the_permalink() ?>"><img src="<?php the_post_thumbnail() ?>" alt=""></a>
-                </div>
-                <a href="<?php the_permalink() ?>">
-                    <h4 class="article-title">
-                        <?php the_title(); ?>
-                    </h4>
-                </a>
-                <p>
-                    <?php the_excerpt() ?>
-                </p>
+        <div class="article">
+            <div class="article-image">
+                <a href="<?php the_permalink() ?>"><img src="<?php the_post_thumbnail() ?>" alt=""></a>
             </div>
-            <?php endwhile; endif; ?>
-
+            <a href="<?php the_permalink() ?>">
+                <h4 class="article-title">
+                    <?php the_title(); ?>
+                </h4>
+            </a>
+            <p>
+                <?php the_excerpt() ?>
+            </p>
         </div>
+        <?php endwhile; endif; ?>
+
+    </div>
 
 
 
@@ -53,9 +54,9 @@ get_header('founder') ?>
 
 
 
-    </main>
+</main>
 
-    <?php get_template_part('includes/mission-sections-page'); ?>
+<?php get_template_part('includes/mission-sections-page'); ?>
 
 
-    <?php get_footer() ?>
+<?php get_footer() ?>
