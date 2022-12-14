0216f8e8bb4fcf92604f36d8153798046e1d21ed
Ralph <ramponsah026@st.ug.edu.gh>
2022-12-14 09:59:11 +0000

    indexed

--- front-page.php
+++ front-page.php
@@ -26,7 +26,7 @@
                 </div>
 
                 <div class="jumbotron-images">
-                    <!-- <img src="<?php _e(get_stylesheet_directory_uri()) ?>/images/1.jpg" class="jumb-img" alt=""> -->
+
                     <img src="<?php _e(get_stylesheet_directory_uri()) ?>/images/founder.jpeg" class="jumb-img" alt="">
                 </div>
             </div>
